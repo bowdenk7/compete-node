8d99f129d48a73f7844cd095598bb27ead6287fe
Bowden Kelly <wilkelly@microsoft.com>
2016-10-11 14:55:49 -0700

    refactored out some unused stuff

--- app.js
+++ app.js
@@ -7,15 +7,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 var wrestle = require('./wrestling');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,8 +19,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
 
 app.use(function(req, res, next) {
 
